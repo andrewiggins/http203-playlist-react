27684fca61758a45782f471b0521eb72a16b9344
Andre Wiggins <andrewiggins@live.com>
2025-01-21 15:29:16 -0500

    Prevent tree shaking on global styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
 import { createRoot } from "react-dom/client";
 import App from "./App/index.tsx";
-import "./styles.module.css";
+import styles from "./styles.module.css";
 import { getPageData } from "./shared/data.ts";
+
+// Prevent this CSS module from being tree-shaken. Since it is actually just
+// global styles, it shouldn't be a module per Vite's default behavior. But I'm
+// leaving it at such to match original source code.
+console.log(styles);
 
 const domRoot = document.getElementById("app");
 if (!domRoot) throw new Error("No root element found");
